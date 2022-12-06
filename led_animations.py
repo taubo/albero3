4ef756b0baea297257a83351d2a6331ae31a5ae7
Mauro Tubiana <mauro.tubiana@gmail.com>
2022-12-06 23:39:02 +0100

    try to simulate animations randomization

--- led_animations.py
+++ led_animations.py
@@ -28,18 +28,31 @@ class LedAnimations():
                 pixel_order=neopixel.RGB, brightness=0.1, auto_write=False)
         self.pixels.fill((0, 0, 0))
 
-        sparkle = Sparkle(self.pixels, 0.04, (100, 100, 0), 10)
-        color_cycle = ColorCycle(self.pixels, 0.5, colors=[(100, 0, 0), (0, 100, 0), (0, 0, 100)])
-        chase = Chase(self.pixels, 0.04, color=(0, 100, 0), size=3, spacing=10)
-        comet = Comet(self.pixels, 0.04, (50, 50, 0))
-        pulse = Pulse(self.pixels, 0.04, (0, 50, 0))
+        sparkle1 = Sparkle(self.pixels, 0.04, (200, 200, 0), 10)
+        sparkle2 = Sparkle(self.pixels, 0.04, (0, 200, 200), 10)
+        color_cycle1 = ColorCycle(self.pixels, 1.5, colors=[(100, 0, 0), (0, 100, 0), (0, 0, 100)])
+        color_cycle2 = ColorCycle(self.pixels, 1.5, colors=[(100, 50, 0), (200, 100, 0), (10, 220, 100)])
+        chase1 = Chase(self.pixels, 0.04, color=(0, 200, 0), size=5, spacing=10)
+        chase2 = Chase(self.pixels, 0.04, color=(200, 0, 0), size=5, spacing=30)
+        comet1 = Comet(self.pixels, 0.04, (50, 50, 0))
+        comet2 = Comet(self.pixels, 0.04, (150, 80, 100), reverse = True)
+        pulse = Pulse(self.pixels, 0.04, (100, 150, 20))
         # sparkle = SparklePulse(self.pixels, 0.05, (100, 100, 100))
         rainbow = Rainbow(self.pixels, 0.04)
-        blink = Blink(self.pixels, 0.5, color.PURPLE)
+        blink = Blink(self.pixels, 0.5, (50, 0, 50))
         rainbow_chase = RainbowChase(self.pixels, 0.1, 3, 10)
 
-        self.animation_array = [sparkle, color_cycle, chase, comet, pulse, rainbow, blink, rainbow_chase]
-        self.anim_obj = AnimationSequence(*self.animation_array, advance_interval=20)
+        self.animation_array = [
+                sparkle1, sparkle2,
+                color_cycle1, color_cycle2,
+                chase1, chase2,
+                comet1, comet2,
+                pulse,
+                rainbow,
+                blink,
+                rainbow_chase
+        ]
+        self.anim_obj = AnimationSequence(*self.animation_array, random_order=True, advance_interval=20)
 
     def next(self):
         self.anim_obj.next()
@@ -60,7 +73,7 @@ class LedAnimations():
         self.animation_state = "Play"
 
     def random(self):
-        self.anim_obj = AnimationSequence(*self.animation_array, advance_interval=20)
+        self.anim_obj = AnimationSequence(*self.animation_array, random_order=True, advance_interval=20)
         self.anim_obj.random()
 
     def animate(self):
